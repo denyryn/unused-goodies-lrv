bd9f0987c8423f15bf7814fdefba607933b5ab47
Deny <denyryn@outlook.com>
2025-02-22 15:15:29 +0700

    style(button): update button styles

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-primary btn-outline']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-outline']) }}>
     {{ $slot }}
 </button>
