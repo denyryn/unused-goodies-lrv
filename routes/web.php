def317645d997357d55b12f0a8dbfafd80e3fe2e
Deny <denyryn@outlook.com>
2025-03-01 21:35:32 +0700

    chore: update routes and assets
    - Updated web routes
    - Added missing public assets

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Enums\RoleEnum;
-use App\Http\Controllers\HomePage;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
+
+use App\Enums\RoleEnum;
+use App\Livewire\AddToCart;
+use App\Application\CartApplication;
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
     'role:admin',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+])
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })
+            ->name('dashboard');
+    });
 
-Route::get('/landing', [HomePage::class, 'index'])->middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->name('landing');
+Route::middleware([])
+    ->group(function () {
+        Route::get('/', [PagesController::class, 'home'])
+            ->name('landing_page');
+        Route::get('/home', [PagesController::class, 'home'])
+            ->name('home_page');
+        Route::prefix('category')->group(function () {
+            Route::get('/', [PagesController::class, 'category'])
+                ->name('category_page');
+            Route::get('/{categorySlug?}product', [ProductController::class, 'index'])
+                ->where('categorySlug', '[a-zA-Z0-9-]*')
+                ->name('product_page');
+        });
+
+        Route::get('/product/{productSlug}', [ProductController::class, 'show'])
+            ->name('product_detail_page');
+    });
+
