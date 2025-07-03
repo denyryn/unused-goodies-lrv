c744f463d55a2ca19f6f42b5ac91394e51335d21
Deny Rianto <denyryn@outlook.com>
2025-07-03 09:05:43 +0700

    refactor(cart): changed old components to the new one

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,27 +1,26 @@
 @php
     $navLinks = [
-    [
-        'name' => 'Home',
-        'href' => route('home_page'),
-        'active' => request()->routeIs('home_page'),
-    ],
-    [
-        'name' => 'Category',
-        'href' => route('category_page'),
-        'active' => request()->routeIs('category_page'),
-    ],
-    [
-        'name' => 'Product',
-        'href' => route('product_page'),
-        'active' => request()->routeIs('product_page'),
-    ],
-    ]
+        [
+            'name' => 'Home',
+            'href' => route('home_page'),
+            'active' => request()->routeIs('home_page'),
+        ],
+        [
+            'name' => 'Category',
+            'href' => route('category_page'),
+            'active' => request()->routeIs('category_page'),
+        ],
+        [
+            'name' => 'Product',
+            'href' => route('product_page'),
+            'active' => request()->routeIs('product_page'),
+        ],
+    ];
 @endphp
 <nav x-data="{ open: false }" class="bg-theme border-gray-200 dark:border-gray-950 shadow">
     <div class="navbar text-theme-invert">
         <div class="navbar-start">
-            <a href="{{ route('home_page') }}"
-                class="btn btn-ghost w-fit h-fit">
+            <a href="{{ route('home_page') }}" class="btn btn-ghost w-fit h-fit">
                 <img class="flex items-center justify-center w-14" src="{{ asset('assets/Logo/logo.svg') }}"
                     alt="{{ config('app.name', 'Unused Goodies') }}">
             </a>
@@ -29,15 +28,15 @@
         <div class="hidden navbar-center sm:flex ">
             <ul class="px-1 font-bold menu menu-horizontal font-rubik space-x-1">
                 @foreach ($navLinks as $link)
-                <li><x-nav-link href="{{ $link['href'] }}" :active="$link['active']">{{ $link['name'] }}</x-nav-link></li>
+                    <li><x-nav-link href="{{ $link['href'] }}" :active="$link['active']">{{ $link['name'] }}</x-nav-link></li>
                 @endforeach
             </ul>
         </div>
         <div class="navbar-end space-x-1">
             @if (\App\Utilities\Permission::isLoggedIn())
-                <livewire:cart.components.floating-cart>
+                <livewire:cart.quick-view-cart>
             @endif
-            
+
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Teams Dropdown -->
                 @if (\App\Utilities\Permission::isLoggedIn() && Laravel\Jetstream\Jetstream::hasTeamFeatures())
@@ -45,11 +44,14 @@
                         <x-dropdown align="right" width="60">
                             <x-slot name="trigger">
                                 <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                    <button type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                                         {{ Auth::user()->currentTeam->name }}
 
-                                        <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                        <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                         </svg>
                                     </button>
                                 </span>
@@ -91,21 +93,28 @@
                     </div>
                 @endif
 
-                @if(\App\Utilities\Permission::isLoggedIn())
+                @if (\App\Utilities\Permission::isLoggedIn())
                     <!-- Settings Dropdown -->
                     <div class="ms-3 relative">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
                                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                    <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                        <img class="size-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    <button
+                                        class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="size-8 rounded-full object-cover"
+                                            src="{{ Auth::user()->profile_photo_url }}"
+                                            alt="{{ Auth::user()->name }}" />
                                     </button>
                                 @else
                                     <span class="inline-flex rounded-md">
-                                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                                             {{ Auth::user()->name }}
-                                            <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            <svg class="ms-2 -me-0.5 size-4" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                             </svg>
                                         </button>
                                     </span>
@@ -140,8 +149,7 @@
                                 <form method="POST" action="{{ route('logout') }}" x-data>
                                     @csrf
 
-                                    <x-dropdown-link href="{{ route('logout') }}"
-                                             @click.prevent="$root.submit();">
+                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
                                         {{ __('Log Out') }}
                                     </x-dropdown-link>
                                 </form>
@@ -150,27 +158,32 @@
                     </div>
                 @else
                     <a href="{{ route('login') }}" class="btn btn-ghost ">
-                        <img class="size-5 dark:invert" src="{{ \App\Utilities\Asset::getFromAssets('Icons/user.svg') }}" alt="User Login">
+                        <img class="size-5 dark:invert"
+                            src="{{ \App\Utilities\Asset::getFromAssets('Icons/user.svg') }}" alt="User Login">
                     </a>
                 @endif
             </div>
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex btn btn-ghost items-center justify-center p-2 rounded-md text-gray-800 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:text-gray-900 dark:focus:text-gray-100 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                    class="inline-flex btn btn-ghost items-center justify-center p-2 rounded-md text-gray-800 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:text-gray-900 dark:focus:text-gray-100 transition duration-150 ease-in-out">
                     <svg class="size-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
-            
+
         </div>
-        
+
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             @if (\App\Utilities\Permission::isLoggedIn())
                 @foreach ($navLinks as $link)
@@ -180,29 +193,31 @@
                 @endforeach
             @endif
         </div>
-        
+
         <!-- Responsive Settings Options -->
         @if (\App\Utilities\Permission::isLoggedIn())
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                 <div class="flex items-center px-4">
                     @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                         <div class="shrink-0 me-3">
-                            <img class="size-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                            <img class="size-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                                alt="{{ Auth::user()->name }}" />
                         </div>
                     @endif
 
                     <div>
-                        <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                        <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}
+                        </div>
                         <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                     </div>
                 </div>
-                
+
                 <div class="mt-3 space-y-1">
                     <!-- Account Management -->
                     <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
                         {{ __('Profile') }}
                     </x-responsive-nav-link>
-                    
+
                     @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                         <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
                             {{ __('API Tokens') }}
@@ -217,8 +232,7 @@
                     <form method="POST" action="{{ route('logout') }}" x-data>
                         @csrf
 
-                        <x-responsive-nav-link href="{{ route('logout') }}"
-                                    @click.prevent="$root.submit();">
+                        <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
                             {{ __('Log Out') }}
                         </x-responsive-nav-link>
                     </form>
@@ -232,7 +246,8 @@
                         </div>
 
                         <!-- Team Settings -->
-                        <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
+                        <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}"
+                            :active="request()->routeIs('teams.show')">
                             {{ __('Team Settings') }}
                         </x-responsive-nav-link>
 
